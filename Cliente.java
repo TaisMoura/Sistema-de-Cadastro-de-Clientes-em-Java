c1cb96afbd109693544de05f06cb2198538e16cd
TaisMoura <tais.moura@pepsico.com>
2026-09-23 12:34:31 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,36 @@
+public class Cliente {
+
+    private String nome;
+    private int idade;
+    private String telefone;
+
+    public Cliente(String nome, int idade, String telefone) {
+        this.nome = nome;
+        this.idade = idade;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
